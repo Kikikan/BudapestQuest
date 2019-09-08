f72788cc2bb71c9c0c873041143994545a0a5ab8
BuildTools <unconfigured@null.spigotmc.org>
2019-09-08 17:24:07 +0200

    karakter szint változó lemaradt whoops

--- app/src/main/java/com/example/budapestquest/Karakterek/Karakter.java
+++ app/src/main/java/com/example/budapestquest/Karakterek/Karakter.java
@@ -5,6 +5,7 @@ import com.example.budapestquest.Targyak.Targy;
 public class Karakter {
 
     public String Name = "";
+    public int LVL = 0;
     public float XP = 0.0f;    // Ákos dokumentumából kiszedett statok
     public int FT = 1000;
     public float HP = 100.0f;
@@ -17,6 +18,6 @@ public class Karakter {
     public Targy[] Felszereles = new Targy[4];  // 0 - fej, 1 - mellkas, 2 - láb, 3 - fegyver
 
     public String GenerateCode() {
-        return Name + "|" + HP + "|" + DMG + "|" + DaP + "|" + DeP + "|" + CR + "|" + DO; //Todo: Felszerelés továbbítása
+        return Name + "|" + LVL + "|" + HP + "|" + DMG + "|" + DaP + "|" + DeP + "|" + CR + "|" + DO; //Todo: Felszerelés továbbítása
     }
 }
