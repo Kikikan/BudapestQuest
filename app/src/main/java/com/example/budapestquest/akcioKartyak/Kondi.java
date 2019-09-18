5c7dc2b80a08004dd0fe9a63431ed53ca216e142
Barkodczy <barkobalazs10@gmail.com>
2019-09-18 17:37:45 +0200

    Kondi és kaszino backendje teljesen készen van. A gamecontrollerbe is bele van kötve márcsak a frontendel kell összekötni amikor készen lesz

--- app/src/main/java/com/example/budapestquest/akcioKartyak/Kondi.java
+++ app/src/main/java/com/example/budapestquest/akcioKartyak/Kondi.java
@@ -6,68 +6,24 @@ public class Kondi
 {
     public static Karakter harciEdzes(Karakter en)
     {
-        int belepo = 100;
+        int belepo = 150;
 
         en.FT -= belepo;
 
-        //TODO KÉRDÉS (optimalizálok): Ha 40 pontja van akkor melyikhez tartozon?
-        if(en.DaP < 40)
-        {
-            en.DaP += 3;
-        }
-        else
-        {
-            en.DaP += 2;
-        }
+        en.DaP += 2;
 
         return en;
     }
 
     public static Karakter kardioEdzes (Karakter en)
     {
-        //TODO KÉRDÉS (optimalizálok): 1 egység legyen az ára?
-        int belepo = 100;
+        int belepo = 150;
 
         en.FT -= belepo;
 
-
-        if(en.DeP < 40)
-        {
-            en.DeP += 6;
-        }
-        else
-        {
-            en.DeP += 4;
-        }
+        en.DeP += 2;
 
         return en;
     }
 
-    public static Karakter vegyesEdzes (Karakter en)
-    {
-        int belepo = 180;
-
-        en.FT -= belepo;
-
-
-        if(en.DeP < 40)
-        {
-            en.DeP += 6;
-        }
-        else
-        {
-            en.DeP += 4;
-        }
-
-        if(en.DaP < 40)
-        {
-            en.DaP += 3;
-        }
-        else
-        {
-            en.DaP += 2;
-        }
-
-        return en;
-    }
 }
