8b11085499db9395321f416a6ac451a20bd58a00
Kikikan <gykrisz00@gmail.com>
2019-09-20 11:31:36 +0200

    munka UI kész, kaszinó UI javítva

--- app/src/main/java/com/example/budapestquest/ActionCards/MunkaAct.java
+++ app/src/main/java/com/example/budapestquest/ActionCards/MunkaAct.java
@@ -1,28 +1,38 @@
 package com.example.budapestquest.ActionCards;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.budapestquest.GameController;
 import com.example.budapestquest.R;
 
-import android.os.Bundle;
-import android.text.Editable;
-import android.widget.EditText;
-
 public class MunkaAct extends AppCompatActivity {
+
+    EditText kor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_munka);
+        kor = findViewById(R.id.korEditText);
+        Button button = findViewById(R.id.munkaButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ButtonMunka();
+            }
+        });
+
     }
 
     public int getDB(){
-        //TODO azt a edittextből kikérni az adatot ami benne van
-        EditText edit = null;//(EditText) findViewById(R.id.);
-        Editable editable = edit.getText();
-        String allTheText = editable.toString();
-        return Integer.parseInt(allTheText);
+        return Integer.parseInt(kor.getText().toString());
     }
 
     public void ButtonMunka()
