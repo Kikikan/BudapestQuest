d412f5686a4ea5ca98a6b9afbd5435aac4e0decb
RadioDreamer <buzgalevi@gmail.com>
2019-09-20 10:53:10 +0200

    //Boltok készen

--- app/src/main/java/com/example/budapestquest/akcioKartyak/Tier2Shop.java
+++ app/src/main/java/com/example/budapestquest/akcioKartyak/Tier2Shop.java
@@ -3,30 +3,31 @@ import com.example.budapestquest.Targyak.Targy;
 import com.example.budapestquest.GameController;
 
 public  class Tier2Shop {
-    Targy [] targyak;
+    Targy[] targyak;
     int capacity;
 
     //Másik két shopra is meg kell csinálni, csak nem tudom hogy Activity lesz-e belőlük
-    public Tier2Shop(){
+    public Tier2Shop() {
         capacity = 4;
-        targyak = new Targy [capacity];
-        feltolt(capacity,targyak);
+        targyak = new Targy[capacity];
+        feltolt(capacity, targyak);
     }
+
     //Feltölti a boltot 4 különböző itemmel
-    public static void feltolt(int capacity, Targy [] test) {
+    public static void feltolt(int capacity, Targy[] test) {
         for (int j = 0; j < capacity; j++) {
             test[j] = Targy.Generate(j, 2);
         }
     }
+
     //Megvesz a 4 item közül egyet
-    public void vasarlas(int index){
-        double actualPrice = targyak[index].item.Price*targyak[index].modifier.PriceWeight;
-        if(GameController.En.FT >= actualPrice) {
+    public void vasarlas(int index) {
+        double actualPrice = targyak[index].item.Price * targyak[index].modifier.PriceWeight;
+        if (GameController.En.FT >= actualPrice) {
             GameController.En.FT -= actualPrice;
             GameController.En.Felszereles[index] = targyak[index];
         }
 
-
-
     }
+}
 
--- app/src/main/java/com/example/budapestquest/akcioKartyak/Tier3Shop.java
+++ app/src/main/java/com/example/budapestquest/akcioKartyak/Tier3Shop.java
@@ -3,30 +3,32 @@ import com.example.budapestquest.Targyak.Targy;
 import com.example.budapestquest.GameController;
 
 public  class Tier3Shop {
-    Targy [] targyak;
+    Targy[] targyak;
     int capacity;
 
     //Másik két shopra is meg kell csinálni, csak nem tudom hogy Activity lesz-e belőlük
-    public Tier3Shop(){
+    public Tier3Shop() {
         capacity = 4;
-        targyak = new Targy [capacity];
-        feltolt(capacity,targyak);
+        targyak = new Targy[capacity];
+        feltolt(capacity, targyak);
     }
+
     //Feltölti a boltot 4 különböző itemmel
-    public static void feltolt(int capacity, Targy [] test) {
+    public static void feltolt(int capacity, Targy[] test) {
         for (int j = 0; j < capacity; j++) {
             test[j] = Targy.Generate(j, 3);
         }
     }
+
     //Megvesz a 4 item közül egyet
-    public void vasarlas(int index){
-        double actualPrice = targyak[index].item.Price*targyak[index].modifier.PriceWeight;
-        if(GameController.En.FT >= actualPrice) {
+    public void vasarlas(int index) {
+        double actualPrice = targyak[index].item.Price * targyak[index].modifier.PriceWeight;
+        if (GameController.En.FT >= actualPrice) {
             GameController.En.FT -= actualPrice;
             GameController.En.Felszereles[index] = targyak[index];
         }
 
 
-
     }
 
+}
