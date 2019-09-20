02c3b47cf65bb90bf5910408393bd4756d783b71
RadioDreamer <buzgalevi@gmail.com>
2019-09-20 10:16:35 +0200

    //BoltActban konstruáltam a Tier1Shoppot

--- app/src/main/java/com/example/budapestquest/ActionCards/BoltAct.java
+++ app/src/main/java/com/example/budapestquest/ActionCards/BoltAct.java
@@ -2,14 +2,16 @@ package com.example.budapestquest.ActionCards;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.budapestquest.R;
+import com.example.budapestquest.akcioKartyak.Tier1Shop;
 
 import android.os.Bundle;
 
 public class BoltAct extends AppCompatActivity {
-
+Tier1Shop bolt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bolt = new Tier1Shop();
         setContentView(R.layout.activity_bolt);
     }
 }
